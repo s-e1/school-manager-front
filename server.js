87189c93f3a102e7dc25095309be1c842a18406e
shneur <49290335+rubiginous@users.noreply.github.com>
2020-12-25 11:17:29 +0200

    cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/school'));
