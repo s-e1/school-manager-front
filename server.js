c04f30b9489f3df5c4284ff30365f1513780635a
shneur <49290335+rubiginous@users.noreply.github.com>
2020-12-25 11:38:55 +0200

    remove cors

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
-app.use(cors());
+// app.use(cors());
 
 // Serve static files....
 app.use(express.static(__dirname + '/dist/school'));
